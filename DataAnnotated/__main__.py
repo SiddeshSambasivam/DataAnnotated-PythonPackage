3a0484e9588c7f6d65d24c3133c00285acb6ea68
Siddesh Sambasivam <plutocrat45@gmail.com>
2021-02-25 05:52:24 +0530

    initial commit

---
+++ DataAnnotated/__main__.py
@@ -0,0 +1,24 @@
+print("Data Annotated")
+
+# Login the first time
+    #  Make a API call the get the JWT token
+    #  Complete the authentication
+    #  Downloads all the data
+
+# Basic check for login
+    #  Basic Features
+        # ShowDatasets()
+            # List all the annotated datasets
+        # fetchDatasets()
+            # Download the dataset
+        # parseDatasets()
+            # give the data in the format provided by the user
+
+def ShowDatasets():
+    pass
+
+def fetchDatasets():
+    pass
+
+def parseDatasets():
+    pass
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# DataAnnotated-PythonPackage
+<h1 align="center" style="font-weight:bold">
+    DataAnnotated
+</h1>
+
 A python package for DataAnnotated platform to provide seamless integration between annotation and development
